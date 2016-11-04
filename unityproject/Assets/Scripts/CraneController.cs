0d4619b176b0637ecc85195d6fdd6c22872e6615
Fredrik Olsson <fo@cego.dk>
2016-11-04 16:24:14 +0100

    Improved rope rendering

--- unityproject/Assets/Scripts/CraneController.cs
+++ unityproject/Assets/Scripts/CraneController.cs
@@ -7,27 +7,31 @@ public class CraneController : MonoBehaviour {
     GameObject currentBlock = null;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         StartCoroutine(SpawnBox());
         this.GetComponent<LineRenderer>().SetPosition(0, this.transform.position);
+        this.GetComponent<LineRenderer>().SetPosition(1, this.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            this.GetComponent<DistanceJoint2D>().connectedBody = null;
-            this.GetComponent<LineRenderer>().SetPosition(1, this.transform.position);
-            StartCoroutine(SpawnBox());
-        }
         if (currentBlock != null)
         {
+            if (Input.GetMouseButtonDown(0))
+            {
+                this.GetComponent<LineRenderer>().SetPosition(1, this.transform.position);
+                this.GetComponent<DistanceJoint2D>().connectedBody = null;
+                currentBlock = null;
+                StartCoroutine(SpawnBox());
+            }
             this.GetComponent<LineRenderer>().SetPosition(1, currentBlock.transform.position);
         }
     }
 
-    IEnumerator SpawnBox() {
+    IEnumerator SpawnBox()
+    {
         yield return new WaitForSeconds(2.0f);
 
         Vector3 pos = this.transform.position;
