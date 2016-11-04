3fdcadba457700b908a79bd28ea96e6832f16d6b
jenspaaske <jenspaaske@gmail.com>
2016-11-04 18:42:06 +0100

    Mark as fallen script on blocks.

---
+++ unityproject/Assets/Scripts/HeightChecker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeightChecker : MonoBehaviour {
+
+    public float height;
+	
+	// Update is called once per frame
+	void Update () {
+
+        GameObject[] blocks = GameObject.FindGameObjectsWithTag("Block");
+
+        height = -200;
+
+        foreach (GameObject block in blocks)
+        {
+            float blockHeight = block.transform.position.y;
+            if (blockHeight > height)
+            {
+                height = blockHeight;
+            }
+        }
+	}
+}
