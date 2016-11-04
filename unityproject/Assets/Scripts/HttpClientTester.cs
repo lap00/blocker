fb19c8b751afc4fd714ef8865fc785eea6894edd
Jakob Svane Knudsen <jakobknudsen@gmail.com>
2016-11-04 22:00:47 +0100

    Add intial http client and tester of same.

---
+++ unityproject/Assets/Scripts/HttpClientTester.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(HttpClient))]
+public class HttpClientTester : MonoBehaviour {
+
+    HttpClient client;
+
+    public void Start()
+    {
+        this.client = GetComponent<HttpClient>();
+    }
+
+    public void OnGUI()
+    {
+        if (GUI.Button(new Rect(50, 100, 100, 20), "Get highscores")) {
+            client.GetHighscores(this.LogHighscores);
+        }
+    }
+
+    private void LogHighscores(HttpClient.HighscoreList list)
+    {
+        foreach (HttpClient.Highscore s in list.highscores)
+            Debug.Log(s.name + ": " + s.height);
+    }
+}
