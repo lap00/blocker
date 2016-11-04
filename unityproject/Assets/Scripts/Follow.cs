4c1549cc28799d2f22ade835662863ea959461b8
Fredrik Olsson <fo@cego.dk>
2016-11-04 20:41:46 +0100

    Added follow script

---
+++ unityproject/Assets/Scripts/Follow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject target;
+    public float offset;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(pos.x, Mathf.Max(3.5f, target.transform.position.y + offset), pos.z);
+    }
+}
