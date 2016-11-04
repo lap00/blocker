fa20b2765cda866ad78a3b228af694916b3dfb64
Jakob Svane Knudsen <jk@cego.dk>
2016-11-04 23:31:19 +0100

    Add placeholder highscore endpoints.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require('express'),
+    bodyParser = require('body-parser');
 
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send(JSON.stringify({foo: 'bar'}));
+app.use(bodyParser.json());
+
+app.get('/highscores', function (req, res) {
+    res.json({
+        highscores: [
+            {
+                name: 'Jakob',
+                height: 1000000000.0
+            },
+            {
+                name: 'Jens',
+                height: 12479173.0
+            }
+        ]
+    });
+});
+
+app.post('/highscores', function (req, res) {
+    console.log(req.body);
+    res.json({
+        message: 'I did nothing.'
+    });
 });
 
 app.listen(8080);
