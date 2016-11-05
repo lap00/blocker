1eac57d619a5f1b33ef516ed6b6a7a11f34ad614
Soren Thorsen <st@cego.dk>
2016-11-05 15:51:46 +0100

    Making back button working in highscore

---
+++ unityproject/Assets/Back.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Back : MonoBehaviour {
+
+	public void BackButton()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+}
