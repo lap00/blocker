da222b3d189faf03ff795467bda27d35f5859b6d
Jakob Svane Knudsen <jakobknudsen@gmail.com>
2016-11-05 20:01:56 +0100

    Add splashes.

---
+++ unityproject/Assets/Scripts/DestroyWhenParticlesEmpty.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class DestroyWhenParticlesEmpty : MonoBehaviour
+{
+    public float gracePeriod = 1f;
+
+    ParticleSystem ps;
+    float t0;
+
+    public void Start()
+    {
+        t0 = Time.time;
+        ps = GetComponent<ParticleSystem>();
+    }
+
+	public void Update ()
+    {
+        if (Time.time - t0 > gracePeriod && ps.particleCount == 0)
+            Destroy(gameObject);
+	}
+}
