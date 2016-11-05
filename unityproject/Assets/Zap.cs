52e63bfbd4a8681ca230ebbc26a5426a659f456a
jenspaaske <jenspaaske@gmail.com>
2016-11-05 09:19:41 +0100

    Added ZapBlock

---
+++ unityproject/Assets/Zap.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zap : MonoBehaviour {
+
+	public void OnCollisionEnter2D (Collision2D coll)
+    {
+        if (coll.gameObject.CompareTag("Block"))
+        {
+            coll.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
