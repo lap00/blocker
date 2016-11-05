8ef7bc1024fc6545b01743e2522e3645a0d503ec
Jakob Svane Knudsen <jk@cego.dk>
2016-11-05 09:44:31 +0100

    Rewrote endpoints and added mongo storage.

--- server/index.js
+++ server/index.js
@@ -1,30 +1,71 @@
 var express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    assert = require('assert'),
+    async = require('async'),
+    mongo = require('./mongo.js');
 
 var app = express();
 
 app.use(bodyParser.json());
 
 app.get('/highscores', function (req, res) {
-    res.json({
-        highscores: [
-            {
-                name: 'Jakob',
-                height: 1000000000.0
-            },
-            {
-                name: 'Jens',
-                height: 12479173.0
-            }
-        ]
+    mongo.go(function (err, db) {
+        db.collection('bests').find({}).sort({height: -1}).limit(5).toArray(function (err, docs) {
+            res.json({ highscores: docs });
+        });
     });
 });
 
-app.post('/highscores', function (req, res) {
-    console.log(req.body);
-    res.json({
-        message: 'I did nothing.'
-    });
+app.post('/states', function (req, res) {
+    async.waterfall([
+        function (callback) {
+            mongo.go(function (err, db) {
+                db.collection('states').updateOne(
+                    { token: req.body.token },
+                    { $set: {
+                        name: req.body.name,
+                        height: req.body.height,
+                        count: req.body.count,
+                        weight: req.body.weight
+                    }},
+                    { upsert: true }
+                );
+                return callback(null);
+            });
+        },
+        function (callback) {
+            mongo.go(function (err, db) {
+                db.collection('bests').findOne(
+                    { name: req.body.name },
+                    null,
+                    null,
+                    function (err, doc) { return callback(null, doc ? doc : req.body); }
+                );
+            });
+        },
+        function (best, callback) {
+            mongo.go(function (err, db) {
+                db.collection('bests').update(
+                    { name: req.body.name },
+                    { $set: {
+                        height: Math.max(req.body.height, best.height),
+                        weight: Math.max(req.body.weight, best.weight),
+                        count:  Math.max(req.body.count,  best.count)
+                    }},
+                    { upsert: true }
+                );
+            });
+            return callback(null);
+        },
+        function (callback) {
+            res.json({});
+        }
+    ]);
+});
+
+mongo.go(function (err, db) {
+    assert.equal(err, null);
+    db.close();
 });
 
 app.listen(8080);
